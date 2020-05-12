8ea34fa9af1678ebabd648f997d0f2ece7300bc9
jbs1340 <onggiabao1998@gmail.com>
2020-05-12 16:49:10 +0700

    fix cors

--- routes/auth.js
+++ routes/auth.js
@@ -4,7 +4,9 @@ const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
 router.post('/login/user', authController.loginUser);
+router.options('/login/user', (req,res)=>res.status(200));
 router.post('/login/admin', authController.loginAdmin);
+router.options('/login/admin', (req,res)=>res.status(200));
 router.post('/register',authController.register);
 router.get('/any',authController.verifyToken,userController.getUsersAny)
 router.get('/all',authController.verifyToken,userController.getUsersAll)
