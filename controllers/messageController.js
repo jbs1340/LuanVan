2ff15454740f14b956feab49beadca43f6c1fbaf
LAPTOP-K4I0BA93\ADMIN <baoog@ghn.vn>
2020-07-18 01:27:06 +0700

    reverse mess

--- controllers/messageController.js
+++ controllers/messageController.js
@@ -3,7 +3,7 @@ var messageDB = require('../models/message')
 var ChatroomDB = require("../models/chatroom");
 const mongoose = require('mongoose')
 
-exports.getMessByIDRoom = (req,res)=>{
+exports.getMessByIDRoom = (req, res) => {
     var idRoom = req.query.idRoom || ""
     var limit = parseInt(req.query.limit) || 1;
     var offset = parseInt(req.query.offset) || 0;
@@ -12,21 +12,22 @@ exports.getMessByIDRoom = (req,res)=>{
     var query = {
         roomID: idRoom
     }
-    ChatroomDB.find({_id: idRoom,"users._id": currentUser._id},2,0,(err,room)=>{
-        if(err)
-            return res.status(500).send({status: 500, message: err.message})
-        if(room.length > 0){
-            messageDB.get(query,limit,offset,false,(err,mess)=>{
-                if(err)
-                    return res.status(500).send({status: 500, message: err.message})
-                if(mess.length > 0){
-                    return res.status(200).send({status: 200, message:"Query successfully", data: mess})
+    ChatroomDB.find({ _id: idRoom, "users._id": currentUser._id }, 2, 0, (err, room) => {
+        if (err)
+            return res.status(500).send({ status: 500, message: err.message })
+        if (room.length > 0) {
+            messageDB.get(query, limit, offset, true, (err, mess) => {
+                if (err)
+                    return res.status(500).send({ status: 500, message: err.message })
+                if (mess.length > 0) {
+                    mess.reverse()
+                    return res.status(200).send({ status: 200, message: "Query successfully", data: mess })
                 } else {
-                    return res.status(404).send({status: 404, message:"NOT FOUND", data: mess})
+                    return res.status(404).send({ status: 404, message: "NOT FOUND", data: mess })
                 }
             })
         } else {
-            return res.status(500).send({status: 500, message: "Bạn không có quyền truy cập"})            
+            return res.status(500).send({ status: 500, message: "Bạn không có quyền truy cập" })
         }
     })
 }
