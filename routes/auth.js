d3586361bfcf73dd88cb3b45f3fa0b257232d478
LAPTOP-K4I0BA93\ADMIN <baoog@ghn.vn>
2020-07-18 15:13:55 +0700

    fix query

--- routes/auth.js
+++ routes/auth.js
@@ -1,14 +1,15 @@
 const express = require('express');
-const router  = express.Router();
+const router = express.Router();
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 const rankController = require('../controllers/rankController');
 
 router.post('/login/user', authController.loginUser);
 router.post('/login/admin', authController.loginAdmin);
-router.post('/register',authController.register);
-router.get('/any',authController.verifyToken,userController.getUsersAny)
-router.get('/all',authController.verifyToken,userController.getUsersAll)
-router.get('/rank_all',rankController.rank_all)
-router.get('/rank_self',authController.verifyToken,rankController.rank_self)
+router.post('/register', authController.register);
+router.get('/any_by_username', authController.verifyToken, userController.getUsersAnyByUsername)
+router.get('/any', authController.verifyToken, userController.getUsersAny)
+router.get('/all', authController.verifyToken, userController.getUsersAll)
+router.get('/rank_all', rankController.rank_all)
+router.get('/rank_self', authController.verifyToken, rankController.rank_self)
 module.exports = router;
