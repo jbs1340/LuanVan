6c5e33b0b8e4facbf082ef68730dd7a6c5c010a0
LAPTOP-K4I0BA93\ADMIN <baoog@ghn.vn>
2020-07-31 14:34:06 +0700

    fix upload

--- controllers/bureauController.js
+++ controllers/bureauController.js
@@ -20,18 +20,18 @@ exports.create = (req, res) => {
         return res.status(500).send({ message: "Thiếu thông tin" });
     }
 
-    BureauDB.getFromName(data.name, (err, data) => {
+    BureauDB.getFromName(data.name, (err, result) => {
         if (err != null) msg = "Lỗi không xác định, không tạo được phòng ban";
-        if (data != null) {
+        if (result != null) {
             msg = "Tên phòng ban đã được đăng ký";
         }
         if (msg == '') {
-            BureauDB.create(data, (err, data) => {
+            BureauDB.create(data, (err, bureau) => {
                 if (err) {
                     var message = msg != null ? msg : err != null ? err.message : null
                     return res.status(406).send({ status: 406, message: message })
                 } else
-                    return res.status(200).send({ status: 200, data: data, message: "Tạo văn phòng ban thành công" })
+                    return res.status(200).send({ status: 200, data: bureau, message: "Tạo văn phòng ban thành công" })
             })
         } else if (msg != '')
             return res.status(406).send({ status: 406, message: msg })
