250d1ee40fa8793c3f44eced53730dfa81b5edbb
LAPTOP-K4I0BA93\ADMIN <baoog@ghn.vn>
2020-08-04 15:28:42 +0700

    auto generate _id

--- models/chatroom.js
+++ models/chatroom.js
@@ -2,38 +2,37 @@ var mongoose = require('mongoose');
 var moment = require('moment')
 
 var chatroomSchema = mongoose.Schema({
-    _id: mongoose.mongo.ObjectID,
     name: String,
     users: Array,
     expired: Date,
     createdTime: Date,
     updatedTime: Date,
-    messages : Array
+    messages: Array
 })
 
 var chatroomModel = mongoose.model("Chatroom", chatroomSchema)
 
-exports.create = async(data,cb)=>{
-    chatroomModel.create(data,(err,room)=>{
-        if(err) return cb(err)
-        return cb(null,room)
+exports.create = async(data, cb) => {
+    chatroomModel.create(data, (err, room) => {
+        if (err) return cb(err)
+        return cb(null, room)
     })
 }
 
-exports.findById = (_id,cb)=>{
-    chatroomModel.findById(_id,(err,room)=>{
-        if(err) return cb(err)
-        return cb(err,room)
+exports.findById = (_id, cb) => {
+    chatroomModel.findById(_id, (err, room) => {
+        if (err) return cb(err)
+        return cb(err, room)
     })
 }
 
-exports.find = (query,limit,offset,cb)=>{
-    chatroomModel.find(query,(err,room)=>{
-        if(err) return cb(err)
-        return cb(err,room)
-    }).limit(limit).skip(offset).sort({updatedTime: -1})
+exports.find = (query, limit, offset, cb) => {
+    chatroomModel.find(query, (err, room) => {
+        if (err) return cb(err)
+        return cb(err, room)
+    }).limit(limit).skip(offset).sort({ updatedTime: -1 })
 }
 
-exports.update_time = (idRoom,cb) =>{
-    chatroomModel.updateOne({_id: idRoom},{updatedTime: moment().format()})
+exports.update_time = (idRoom, cb) => {
+    chatroomModel.updateOne({ _id: idRoom }, { updatedTime: moment().format() })
 }
